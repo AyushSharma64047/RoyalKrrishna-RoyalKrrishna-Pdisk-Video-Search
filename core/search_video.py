42eeb19466db64a01c1b98c6f0c4cbcfd740e2b7
Aayush64047Developer <106085120+AyushSharma64047@users.noreply.github.com>
2023-04-10 18:51:02 +0530

    Update search_video.py

--- core/search_video.py
+++ core/search_video.py
@@ -7,21 +7,21 @@ from core.get_cookies import (
     get_cookies,
     set_cookies
 )
-from core.login import VIVDisk_login
+from core.login import vivdisk_login
 
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36"
 search_api = "https://vivdisk.com/search.php?api=d16088fc465567b77c4fa26e2c3781d5dc63ca64#dDirName=true"
 
 
-async def search_vivdisk_videos(query: str, username: str, password: str) -> Union[dict, Exception]:
+async def search_vivdisk_videos(query: str, email: str, password: str) -> Union[dict, Exception]:
     try:
         cookies = await get_cookies(username, password)
         response = requests.get(search_api.format(parse.quote(query)), cookies={"Cookie": cookies}, headers={"User-Agent": user_agent})
         data = response.json()
         if data["msg"] == "Please login again":
-            user_id, cookies = await pdisk_login(username, password)
+            user_id, cookies = await vivdisk_login(username, password)
             await set_cookies({
-                "username": username,
+                "username": email,
                 "password": password,
                 "user_id": user_id,
                 "cookies": cookies
